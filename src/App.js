b29da4fa15c008c93caef05b29ddd569423010bf
unbornharry <unbornharry@yahoo.com>
2018-02-14 22:23:29 -0700

    Added Sidemenu. Yet to complete full list of items in the menu.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,74 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import Accordion from 'react-tiny-accordion';
+import {SideMenu, Item} from 'react-sidemenu';
 import './App.css';
+
+const items = [
+  {divider: true, label: 'Our Products', value: 'main-nav'},
+    {label: 'Fire Extinguishers', value: 'fireExtinguishers', icon: 'fa-fire-extinguisher',
+      children: [
+        {label: 'Powder Fire Extinguishers', value: 'powderFireExtinguishers'},
+        {label: 'Clean Agent Fire Extinguishers', value: 'cleanAgentFireExtinguishers'},
+        {label: 'Water Fire Extinguishers', value: 'waterFireExtinguishers'},
+        {label: 'Foam Fire Extinguishers', value: 'foamFireExtinguishers'},
+        {label: 'Carbon dioxide Fire Extinguishers', value: 'carbonDioxideFireExtinguishers'},
+        {label: 'Water Mist Fire Extinguishers', value: 'waterMistFireExtinguishers'},
+        {label: 'Backup Trolley Mount Fire Extinguishers', value: 'backupTrolleyFireExtinguishers'},
+        {label: 'Automatic Fire Extinguishers', value: 'automaticFireExtinguishers'}
+        ]
+    },
+    {label: 'Fire Hydrant System', value: 'fireHydrantSystem', icon: 'fa-automobile',
+    children: [
+        {label: 'Yard Fire Hydrant System', value: 'yardFireHydrantSystem'},
+        {label: 'Hose Reel System', value: 'hoseReelSystem'},
+        {label: 'Down Corner System', value: 'downCornerSystem'},
+        {label: 'Dry Riser System', value: 'dryRiserSystem'},
+        {label: 'Wet Riser System', value: 'wetRiserSystem'},
+        {label: 'Automatic Sprinkler System', value: 'automaticSprinklerSystem'}
+        ]
+    },
+    {label: 'Smoke And Fire Detection System', value: 'smokeAndFireDetectionSystem', icon: 'fa-automobile',
+    children: [
+        {label: 'Conventional Fire Alarm System', value: 'conventionalFireAlarmSystem'},
+        {label: 'Addressable Fire Alarm System', value: 'addressableFireAlarmSystem'},
+        {label: 'Stand Alone Fire Alarm System', value: 'standAloneFireAlarmSystem'}
+        ]
+    },
+    {label: 'Escape Signs And Directional Lighting System', value: 'escapeSignsAndDirectionalLightingSystem', icon: 'fa-automobile',
+    children: [
+        {label: 'Photo Luminescent Sign Boards', value: 'conventionalFireAlarmSystem'},
+        {label: 'LED Sign Boards', value: 'ledSignBoards'},
+        {label: 'Normal Sign Boards', value: 'normalSignBoards'},
+        {label: 'Emergency Exit Lights', value: 'emergencyExitLights'}
+        ]
+    },
+    {label: 'Safety Suits', value: 'escapeSignsAndDirectionalLightingSystem', icon: 'fa-automobile',
+    children: [
+        {label: 'Photo Luminescent Sign Boards', value: 'conventionalFireAlarmSystem'},
+        {label: 'LED Sign Boards', value: 'ledSignBoards'},
+        {label: 'Normal Sign Boards', value: 'normalSignBoards'},
+        {label: 'Emergency Exit Lights', value: 'emergencyExitLights'}
+        ]
+    },
+    {label: 'Personal Protective Equipments', value: 'safetySuits', icon: 'fa-automobile',
+    children: [
+        {label: 'Photo Luminescent Sign Boards', value: 'conventionalFireAlarmSystem'},
+        {label: 'LED Sign Boards', value: 'ledSignBoards'},
+        {label: 'Normal Sign Boards', value: 'normalSignBoards'},
+        {label: 'Emergency Exit Lights', value: 'emergencyExitLights'}
+        ]
+    },
+    {label: 'Suppression Systems', value: 'escapeSignsAndDirectionalLightingSystem', icon: 'fa-automobile',
+    children: [
+        {label: 'Photo Luminescent Sign Boards', value: 'conventionalFireAlarmSystem'},
+        {label: 'LED Sign Boards', value: 'ledSignBoards'},
+        {label: 'Normal Sign Boards', value: 'normalSignBoards'},
+        {label: 'Emergency Exit Lights', value: 'emergencyExitLights'}
+        ]
+    },
+  {divider: true, label: 'Motors', value: 'motors-nav'},
+    {label: 'item 3', value: 'item3', icon: 'fa-beer'}
+];
 
 class App extends Component {
   render() {
@@ -29,16 +96,10 @@ class App extends Component {
           <label for="info" class='fa fa-info'>Suppression System</label>
 
           </nav>
-
-          <Accordion className='accordion'>
-              <div data-header="Title">
-                  Content
-              </div>
-              <div data-header="Title">
-                  Content
-              </div>
-          </Accordion>
         </section>
+        <div>
+          <SideMenu items={items}/>
+        </div>
       </div>
     );
   }
